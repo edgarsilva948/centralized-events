041e6c4556105e735071bddcd7ce26606f44a9a4
Edgar Costa <filhedga@amazon.com>
2021-05-10 17:34:02 -0300

    Fixing lambda code and deploy buckets

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # Stack bucket 
-DEST_BUCKET='centralized-events-blog-audit'
+HUB_DEST_BUCKET='centralized-events-blog-audit'
+SPOKE_DEST_BUCKET='centralized-events-blog-audit'
 
 # Stack region
 REGION='us-east-1'
@@ -15,7 +16,6 @@ SPOKE_PARAM_FILE='spoke-account.json'
 SPOKE_ACCOUNT_STACK_NAME='spoke-events'
 SPOKE_ACCOUNT_TEMPLATE_FILE='spoke-account.yaml'
 
-echo "Setting the destination bucket to: " $DEST_BUCKET
 
 if [ ! -d "output" ]
   then
@@ -25,35 +25,39 @@ fi
 
 if [ "$1" == "hub-account" ]
   then
+    echo "Setting the destination bucket to: " $HUB_DEST_BUCKET
+    echo "Setting the regio to: " $REGION    
     echo 'Building the template stack'
     aws cloudformation package \
         --region $REGION \
         --template-file templates/${HUB_ACCOUNT_TEMPLATE_FILE} \
-        --s3-bucket $DEST_BUCKET \
+        --s3-bucket $HUB_DEST_BUCKET \
         --output-template-file output/${HUB_ACCOUNT_TEMPLATE_FILE}
     echo 'Deploying the stack in the HUB Account'
     aws cloudformation deploy \
         --region $REGION \
         --template-file output/${HUB_ACCOUNT_TEMPLATE_FILE} \
         --capabilities CAPABILITY_NAMED_IAM \
-        --s3-bucket ${DEST_BUCKET} \
+        --s3-bucket ${HUB_DEST_BUCKET} \
         --parameter-overrides \
         $(jq -r '.Parameters | keys[] as $k | "\($k)=\(.[$k])"' parameters/${HUB_PARAM_FILE}) \
         --stack-name ${HUB_ACCOUNT_STACK_NAME}
 elif [ "$1" == "spoke-account" ]
   then
+    echo "Setting the destination bucket to: " $SPOKE_DEST_BUCKET
+    echo "Setting the regio to: " $REGION    
     echo 'Building the template stack'
     aws cloudformation package \
         --region $REGION \
         --template-file templates/${SPOKE_ACCOUNT_TEMPLATE_FILE} \
-        --s3-bucket $DEST_BUCKET \
+        --s3-bucket $SPOKE_DEST_BUCKET \
         --output-template-file output/${SPOKE_ACCOUNT_TEMPLATE_FILE}
     echo 'Deploying the stack in the Spoke Account'
     aws cloudformation deploy \
         --region $REGION \
         --template-file output/${SPOKE_ACCOUNT_TEMPLATE_FILE} \
         --capabilities CAPABILITY_NAMED_IAM \
-        --s3-bucket ${DEST_BUCKET} \
+        --s3-bucket ${SPOKE_DEST_BUCKET} \
         --parameter-overrides \
         $(jq -r '.Parameters | keys[] as $k | "\($k)=\(.[$k])"' parameters/${SPOKE_PARAM_FILE}) \
         --stack-name ${SPOKE_ACCOUNT_STACK_NAME}
